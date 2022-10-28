dcb64523dcb8b9d919194e508a4364d84cf46007
Krisna Dewa <59174531+krisdewa@users.noreply.github.com>
2022-10-28 20:52:44 +0700

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,16 +2,18 @@
     <div>
         <nav class="navbar navbar-expand-md navbar-warning fixed-top bg-warning py-3">
             <div class="container">
-                <a class="navbar-brand" href="#">AL-QUR'AN ONLINE</a>
+                <router-link :to="{ name: 'home' }" class="navbar-brand" aria-current="page">AL-QUR'AN ONLINE
+                </router-link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
+                        <!-- <li class="nav-item">
                             <router-link :to="{ name: 'home' }" class="nav-link" aria-current="page">HOME</router-link>
                         </li>
+                        <a href="" class="nav-link">CONTRIBUTE</a> -->
                     </ul>
                     <a href="https://krisproject.my.id" class="btn btn-success float-end" target="_blank">KRISPROJECT 🔗
                     </a>
